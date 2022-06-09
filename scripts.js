799b4cd35544ba305d2603ee92cadc9ffaa631d1
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-06-08 20:52:24 -0400

    feat: added and configured ScrollReveal lib / function showing the elements we want as parameters

--- scripts.js
+++ scripts.js
@@ -1,18 +1,31 @@
-function onScroll(){
-    const navigation = document.querySelector('#navigation');    
-    if(scrollY > 0 ) {
-        navigation.classList.add('scroll');
-    }else {
-        navigation.classList.remove('scroll');
-    }  
-};
+function onScroll() {
+  const navigation = document.querySelector("#navigation");
+  console.log(navigation, scrollY);
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
 
 const bodyElement = document.body;
 
-function onOpenMenu(){
-    bodyElement.classList.add('menu-expanded');
-};
-
-function onCloseMenu(){
-    bodyElement.classList.remove('menu-expanded');
+function onOpenMenu() {
+  bodyElement.classList.add("menu-expanded");
 }
+
+function onCloseMenu() {
+  bodyElement.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+    origin: 'top', 
+    distance: '30px', 
+    duration: 700}).reveal(`
+    #home,
+    #home img,
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card`
+    );
